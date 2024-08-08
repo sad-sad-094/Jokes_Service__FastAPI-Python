24f0a2bdcb3f6ca4ec6b54f4a65b0dce65eb3707
Sebastian Aguirre Duque <sadw621@gmail.com>
2024-08-08 16:28:40 -0500

    :sparkles: feat(main) Adding response classes and responses into methods.

--- app/api/routes/jokes.py
+++ app/api/routes/jokes.py
@@ -1,54 +1,31 @@
-from typing import Annotated
-
-import jwt
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jwt.exceptions import InvalidTokenError
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
-from app.utils import utils
-from app.infra.postgres.crud import jokes_crud
 from app.schemas import jokes
 from app.infra.requests.client import get_random_joke
+from app.infra.postgres.crud import jokes_crud
 from app.infra.postgres.database import get_db
+from app.api.routes.auth import get_current_user_id
 
 
 router = APIRouter()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
-
-
-async def get_current_user_id(
-    token: Annotated[str, Depends(oauth2_scheme)],
-    db: Session = Depends(get_db)
-):
-    
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-    )
-
-
-    try:
-        payload = jwt.decode(token, utils.SECRET_KEY, algorithms=[utils.ALGORITHM])
-        user_id: int = payload.get("user_id")
-
-        if user_id is None:
-            raise credentials_exception
-        
-    except InvalidTokenError:
-        raise credentials_exception
-    return user_id
 
 
 @router.post(
     "/new",
     summary="Get a new joke" ,
     status_code=status.HTTP_201_CREATED,
+    response_class=JSONResponse,
+    responses={
+        201: {"description": "Joke successfully created."},
+        500: {"description": "Internal server error."},
+    },
     response_model=jokes.Joke
 )
 async def create_joke(
-    user_id: Annotated[int, Depends(get_current_user_id)],
+    # user_id: Annotated[int, Depends(get_current_user_id)],
+    user_id: int = Depends(get_current_user_id),
     db: Session = Depends(get_db)
 ):
     new_joke = get_random_joke(user_id)
@@ -65,10 +42,15 @@ async def create_joke(
 @router.get(
     "",
     summary="Get all jokes from an registered user",
+    response_class=JSONResponse,
+    responses={
+        200: {"description": "Successful response."},
+    },
     response_model=list[jokes.JokesRequest]
 )
 async def read_user_jokes(
-    user_id: Annotated[int, Depends(get_current_user_id)],
+    # user_id: Annotated[int, Depends(get_current_user_id)],
+    user_id: int = Depends(get_current_user_id),
     skip: int = 0,
     limit: int = 10,
     db: Session = Depends(get_db)
--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from app.utils import utils
@@ -12,13 +13,16 @@ from app.infra.postgres.database import get_db
 
 router = APIRouter()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
-
 
 @router.post(
     "/register",
     summary="Create a new user",
     status_code=status.HTTP_201_CREATED,
+    response_class=JSONResponse,
+    responses={
+        201: {"description": "User successfully created."},
+        400: {"description": "Email already registered or passwords does not match."},
+    },
     response_model=users.User
 )
 async def create_user(user: users.UserCreate, db: Session = Depends(get_db)):
@@ -40,7 +44,15 @@ async def create_user(user: users.UserCreate, db: Session = Depends(get_db)):
     return users_crud.create_user(db=db, user=user)
 
 
-@router.post("/login", summary="Login a user", response_model=users.Token)
+@router.post("/login",
+    summary="Login a user",
+    response_class=JSONResponse,
+    responses={
+        200: {"description": "User successfully authorized."},
+        401: {"description": "Email has been not registered or incorrect password."},
+    },
+    response_model=users.Token
+)
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Session = Depends(get_db)
@@ -63,10 +75,14 @@ async def login(
 
 
 @router.get(
-        "",
-        summary="Get all registered users",
-        response_model=list[users.UsersRequest]
-    )
+    "",
+    summary="Get all registered users",
+    response_class=JSONResponse,
+    responses={
+        200: {"description": "Successful response."},
+    },
+    response_model=list[users.UsersRequest]
+)
 async def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     users = users_crud.get_users(db, skip=skip, limit=limit)
     return users
